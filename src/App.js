d911aa9fe18b7ce3ca1ca227ea50e22c17ebc793
agusgit <agustinalgo@yahoo.com>
2020-07-06 20:23:10 -0300

    Add custom colors

--- src/App.js
+++ src/App.js
@@ -3,21 +3,58 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Faq from "./pages/Faq";
 import Home from "./pages/Home";
+import {createMuiTheme, MuiThemeProvider, responsiveFontSizes} from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+let theme = createMuiTheme({
+    palette: {
+        type: 'light',
+        background: {
+            default: '#FFFAF0',
+        },
+        primary: {
+            main: '#f2513f',
+        },
+        secondary: {
+            main: '#25a1ba',
+        },
+        text: {
+            // primary: '#424B54',
+        }
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+    spacing: 5,
+    typography: {
+        useNextVariants: true,
+        h3: {
+            fontWeight: 2000,
+        },
+        subtitle1: {
+            fontSize: 20,
+            fontWeight: 500,
+            fontStyle: 'italic',
+        },
+    },
+});
+theme = responsiveFontSizes(theme);
 
 function App() {
-  return (
-      <div className="App">
-          <BrowserRouter>
-              <div>
-                  <Switch>
-                      <Route exact path="/home" component={Home} />
-                      <Route exact path="/faq" component={Faq} />
-                  </Switch>
-              </div>
-          </BrowserRouter>
-
-      </div>
-  );
+    return (
+        <div className="App">
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline/>
+                <BrowserRouter>
+                    <div>
+                        <Switch>
+                            <Route exact path="/home" component={Home}/>
+                            <Route exact path="/faq" component={Faq}/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </MuiThemeProvider>
+        </div>
+    );
 }
 
 export default App;
