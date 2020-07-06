7af787b9823454bfdea899218c6a454917f04eed
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-07-05 23:59:59 -0300

    route

---
+++ src/pages/Faq.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Faq extends React.Component{
+
+    componentDidMount(){
+        //TODO buscar preguntas?
+    }
+
+    render() {
+        return(
+            <div >
+                <h1>
+                    Aca respuesta a : Como comprar? Como pagar? Hacen envios?
+                </h1>
+            </div>
+
+        );
+    }
+}
+
+export default Faq;
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class   Home extends React.Component{
+
+    componentDidMount(){
+        //TODO buscar preguntas?
+    }
+
+    render() {
+        return(
+            <div >
+                <h1>
+                   HOME
+                </h1>
+            </div>
+
+        );
+    }
+}
+
+
+export default Home;
