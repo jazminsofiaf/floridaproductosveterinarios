c17ece31a72be67f6e04924217066efeb5fc9b67
agusgit <agustinalgo@yahoo.com>
2020-07-19 22:27:43 -0300

    Remove slash

--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -2,7 +2,7 @@ class Service {
     public BACKEND_URL: string;
 
     constructor() {
-        this.BACKEND_URL = 'https://morita-backend.herokuapp.com/api/';
+        this.BACKEND_URL = 'https://morita-backend.herokuapp.com/api';
     }
 }
 
