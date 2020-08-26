6e27fe423efac1d5e103dc51696d19a715989a89
agusgit <agustinalgo@yahoo.com>
2020-08-26 19:17:40 -0300

    Add supplier product price info

--- src/app/views/supplierOrder/SupplierProductRow.js
+++ src/app/views/supplierOrder/SupplierProductRow.js
@@ -8,6 +8,65 @@ import Button from "@material-ui/core/Button";
 import CreateProductLink from "../productsLinker/create-product-link";
 import DialogContent from "@material-ui/core/DialogContent";
 import Dialog from "@material-ui/core/Dialog";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 150,
+    },
+});
+
+function createData(priceList, pack, min, bonus, final, unitCost) {
+    return { priceList, pack, min, bonus, final, unitCost };
+}
+
+function PriceDetail(props) {
+    const {item} = props;
+    const classes = useStyles();
+
+    const pack = item.pack_amount ? item.pack_amount : 1;
+    const min = item.amount_for_bonus ? item.amount_for_bonus : 0;
+    const bonus = item.bonus_amount ? item.bonus_amount : 0;
+
+    const rows = [
+        createData(item.list_price, pack, min, bonus, item.price, item.unit_cost),
+    ];
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} size="small" aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>PL</TableCell>
+                        <TableCell>#Pack</TableCell>
+                        <TableCell>#MinBonus</TableCell>
+                        <TableCell>#Bonus</TableCell>
+                        <TableCell>$Final</TableCell>
+                        <TableCell>$c/u.</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.priceList}>
+                            <TableCell>{row.priceList}</TableCell>
+                            <TableCell>{row.pack}</TableCell>
+                            <TableCell>{row.min}</TableCell>
+                            <TableCell>{row.bonus}</TableCell>
+                            <TableCell>${row.final}</TableCell>
+                            <TableCell>${row.unitCost}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
 
 
 function SupplierProductRow(props) {
@@ -31,8 +90,9 @@ function SupplierProductRow(props) {
     return (
         <Paper className="item-box">
             <Grid container spacing={1} style={{textAlign: "left"}}>
-                <Grid item xs={6} sm={12} className="product-title">{item.name}</Grid>
-                <Grid item xs={6} sm={9} className="product-price">$ {item.price}</Grid>
+                <Grid item xs={6} sm={9} className="product-title">{item.name}</Grid>
+                <Grid item xs={6} sm={3} className="product-price">$ {item.price}</Grid>
+                <Grid item xs={12} sm={9}><PriceDetail item={item}/></Grid>
                 <Grid item xs={6} sm={3}>
                     {item.product_id ?
                         AmountField({product: item, addToCart})
